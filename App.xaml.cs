33d510769dff77855aba5580f4eb796d02cb4a64
Israel Hammon <ihammo5@wgu.edu>
2023-09-25 20:02:36 -0400

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace UniversityApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace UniversityApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
