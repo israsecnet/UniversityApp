353ac12aaca13db44d53e4e844032049e4693da5
Israel Hammon <ihammo5@wgu.edu>
2023-09-25 23:10:41 -0400

    Complete wireframe for B2 requirements

---
+++ CourseView.xaml.cs
@@ -0,0 +1,9 @@
+namespace UniversityApp;
+
+public partial class CourseView : ContentPage
+{
+	public CourseView()
+	{
+		InitializeComponent();
+	}
+}
--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,10 +1,17 @@
-﻿namespace UniversityApp
+﻿using System.Windows.Input;
+
+namespace UniversityApp
 {
     public partial class MainPage : ContentPage
     {
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        private async void Button_Clicked(object sender, EventArgs e)
+        {
+            await Navigation.PushAsync(new CourseView());
         }
     }
 }
